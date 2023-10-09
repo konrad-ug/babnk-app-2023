f3f6713b73f262bd2d79a10540b76a0848513f9c
konrad <konrad@tyk.io>
2023-10-09 16:39:17 +0200

    lab1 all features

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -5,9 +5,41 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "89092909876")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, "89092909876", "pesel nie został zapisany!")
 
+    def test_tworzenie_konta_z_niepoprawnym_pesel(self):
+        konto = Konto("Dariusz", "Januszewski", "89092909874ddd")
+        self.assertEqual(konto.pesel, "niepoprawny pesel")
+
+    def test_tworzenie_konta_z_poprawnym_kodem(self):
+        konto = Konto("Dariusz", "Januszewski", "89092909876", "PROM_123")
+        self.assertEqual(konto.saldo, 50, "Saldo nie jest poprawne")
+
+    def test_too_long_promo_code(self):
+        konto = Konto("Dariusz", "Januszewski", "89092909876", "PROM_1234")
+        self.assertEqual(konto.saldo, 0, "Saldo nie jest poprawne")
+
+    def test_too_short_promo_code(self):
+        konto = Konto("Dariusz", "Januszewski", "89092909876", "PROM_12")
+        self.assertEqual(konto.saldo, 0, "Saldo nie jest poprawne")
+    
+    def test_incorrect_promo_code(self):
+        konto = Konto("Dariusz", "Januszewski", "89092909876", "AAA_124")
+        self.assertEqual(konto.saldo, 0, "Saldo nie jest poprawne")
+
+    def test_born_after_50_correct_code(self):
+        konto = Konto("Dariusz", "Januszewski", "03292909875", "PROM_123")
+        self.assertEqual(konto.saldo, 50, "Saldo nie jest poprawne")
+
+    def test_born_after_50_incorrect_code(self):
+        konto = Konto("Dariusz", "Januszewski", "50092909876", "PROM_1234")
+        self.assertEqual(konto.saldo, 0, "Saldo nie jest poprawne")
+
+    def test_born_after_2000_incorrect_code(self):
+        konto = Konto("Dariusz", "Januszewski", "04092909876", "PROM_123")
+        self.assertEqual(konto.saldo, 0, "Saldo nie jest poprawne")
     #tutaj proszę dodawać nowe testy
